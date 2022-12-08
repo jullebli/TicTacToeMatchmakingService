cc22df3a4cf6ce6b88bbf961a8bb16c12cbe7361
vornsami <sami.vornanen@helsinki.fi>
2022-12-08 17:29:09 +0200

    main loop

--- MatchmakingService/src/main/java/com/mycompany/matchmakingservice/Main.java
+++ MatchmakingService/src/main/java/com/mycompany/matchmakingservice/Main.java
@@ -5,10 +5,48 @@
  */
 package com.mycompany.matchmakingservice;
 
+import com.mycompany.matchmakingservice.networking.MulticastPublisher;
+import com.mycompany.matchmakingservice.networking.MulticastReceiver;
+import com.mycompany.matchmakingservice.networking.NetworkPublisher;
+import com.mycompany.matchmakingservice.networking.UserBuffer;
+import java.io.IOException;
+import java.util.List;
+import java.util.Queue;
+
 /**
  *
  * @author bergmjul
  */
 public class Main {
+    private static final int GAME_SIZE = 4;
+    private static final String HOST_LOCATION = "230.0.0.0";
+    private static final String TARGET_LOCATION = "240.0.0.0";
+    private static boolean RUNNING = true;
     
+    public static void main(String[] args) {
+        
+        MulticastReceiver receiver = new MulticastReceiver();
+        receiver.start();
+        
+        NetworkPublisher publisher = new MulticastPublisher();
+        
+        Queue<String> list = UserBuffer.getInstance();
+        try {
+        while(RUNNING) {
+            if(list.size() >= GAME_SIZE) {
+                try {
+                    publisher.send(TARGET_LOCATION, HOST_LOCATION);
+                    for(int i=0;i<GAME_SIZE;i++) list.poll();
+                    
+                } catch (IOException e) {
+                    System.out.println(e);
+                }
+            }
+            
+            Thread.sleep(100);
+        }} catch (InterruptedException e) {
+            System.out.println(e);
+        }
+        
+    }
 }
---
+++ MatchmakingService/src/main/java/com/mycompany/matchmakingservice/networking/MulticastPublisher.java
@@ -0,0 +1,45 @@
+
+package com.mycompany.matchmakingservice.networking;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+/**
+ *
+ * @author bergmjul
+ */
+public class MulticastPublisher implements NetworkPublisher{
+    private DatagramSocket socket;
+    private InetAddress group;
+    private byte[] buf;
+
+    /**
+     * Multicasts a message to a default IP address 230.0.0.0
+     * @param multicastMessage
+     * @throws IOException
+     */
+    public void multicast(String multicastMessage) throws IOException {
+        socket = new DatagramSocket();
+        group = InetAddress.getByName("230.0.0.0");
+        buf = multicastMessage.getBytes();
+
+        DatagramPacket packet 
+          = new DatagramPacket(buf, buf.length, group, 4446);
+        socket.send(packet);
+        socket.close();
+    }
+    
+    @Override
+    public void send(String multicastMessage, String host) throws IOException {
+        socket = new DatagramSocket();
+        group = InetAddress.getByName(host);
+        buf = multicastMessage.getBytes();
+
+        DatagramPacket packet 
+          = new DatagramPacket(buf, buf.length, group, 4446);
+        socket.send(packet);
+        socket.close();
+    }
+}
