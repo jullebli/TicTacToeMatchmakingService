54adad2f13faecb44b5fd091bcc2d994b95db634
Julia Bergman <julia.bergman@helsinki.fi>
2022-12-18 00:27:58 +0200

    Socket connection works with one client

--- MatchmakingService/src/main/java/com/mycompany/matchmakingservice/Main.java
+++ MatchmakingService/src/main/java/com/mycompany/matchmakingservice/Main.java
@@ -8,49 +8,56 @@ package com.mycompany.matchmakingservice;
 import com.mycompany.matchmakingservice.networking.MulticastPublisher;
 import com.mycompany.matchmakingservice.networking.MulticastReceiver;
 import com.mycompany.matchmakingservice.networking.NetworkPublisher;
+import com.mycompany.matchmakingservice.networking.SocketConnections;
 import com.mycompany.matchmakingservice.networking.UserBuffer;
 import java.io.IOException;
 import java.util.List;
 import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author bergmjul
  */
 public class Main {
-    private static final int GAME_SIZE = 4;
+
+    private static final int GAME_SIZE = 2;
     private static final String HOST_LOCATION = "230.0.0.0";
     private static final String TARGET_LOCATION = "230.0.0.1";
     private static boolean RUNNING = true;
-    
+
     public static void main(String[] args) {
-        
-        MulticastReceiver receiver = new MulticastReceiver();
-        receiver.start();
-        
-        NetworkPublisher publisher = new MulticastPublisher();
-        
-        Queue<String> list = UserBuffer.getInstance();
+
+        //MulticastReceiver receiver = new MulticastReceiver();
+        //receiver.start();
+        //NetworkPublisher publisher = new MulticastPublisher();
+        //Queue<String> list = UserBuffer.getInstance();
+        //try {
+        //while(RUNNING) {
+        //    if(list.size() >= GAME_SIZE) {
+        //        try {
+        //          String message = TARGET_LOCATION;
+        //        for(int i=0;i<GAME_SIZE;i++) message += ";" + list.poll();
+
+        //    publisher.send(message, HOST_LOCATION);
+        //here should send to every needed socket the multicastIP
+        //    System.out.println("Sent game to " + TARGET_LOCATION);
+        //        } catch (IOException e) {
+        //            System.out.println(e);
+        //        }
+        //    }
+        //    Thread.sleep(100);
+        //}} catch (InterruptedException e) {
+        //    System.out.println(e);
+        //}
+        SocketConnections sockets = new SocketConnections();
         try {
-        while(RUNNING) {
-            if(list.size() >= GAME_SIZE) {
-                try {
-                    String message = TARGET_LOCATION;
-                    for(int i=0;i<GAME_SIZE;i++) message += ";" + list.poll();
-                    
-                    publisher.send(message, HOST_LOCATION);
-                    //here should send to every needed socket the multicastIP
-                    System.out.println("Sent game to " + TARGET_LOCATION);
-                    
-                } catch (IOException e) {
-                    System.out.println(e);
-                }
-            }
-            
-            Thread.sleep(100);
-        }} catch (InterruptedException e) {
-            System.out.println(e);
+            System.out.println("Try sockets.start(6666)");
+            sockets.start(6666);
+        } catch (IOException e) {
+            System.out.println("Exception: " + e);
         }
-        
+
     }
 }
--- MatchmakingService/src/main/java/com/mycompany/matchmakingservice/networking/SocketConnections.java
+++ MatchmakingService/src/main/java/com/mycompany/matchmakingservice/networking/SocketConnections.java
@@ -9,8 +9,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,12 +23,19 @@ import java.util.logging.Logger;
 public class SocketConnections {
 
     private ServerSocket matchmakerSocket;
+    private ArrayList<String> clientIPs;
 
     public void start(int port) throws IOException {
-        matchmakerSocket = new ServerSocket(port);
+        System.out.println("SocketConnections.start");
+        clientIPs = new ArrayList<>();
+        matchmakerSocket = new ServerSocket(port, 10, InetAddress.getByName("127.0.0.1"));
         while (true) {
             new clientHandler(matchmakerSocket.accept()).start();
         }
+    }
+
+    public void addClientIP(String clientIP) {
+        clientIPs.add(clientIP);
     }
 
     public void stop() throws IOException {
@@ -38,9 +47,11 @@ public class SocketConnections {
         private Socket clientSocket;
         private PrintWriter out;
         private BufferedReader in;
+        private ServerSocket matchmakerSocket;
 
         public clientHandler(Socket clientSocket) {
             this.clientSocket = clientSocket;
+            //this.matchmakerSocket = matchmakerSocket;
         }
 
         public void run() {
@@ -51,16 +62,22 @@ public class SocketConnections {
 
                 String inputLine;
                 while ((inputLine = in.readLine()) != null) {
-                    System.out.println("Matchmaker received: " + inputLine);
+                    System.out.println("Matchmaker received message: " + inputLine);
                     if (inputLine.equals("")) {
-                        out.println("bye");
+                        sendMessage("bye");
                         break;
                     } else if (inputLine.equals("hello matchmaker")) {
-                        out.println("hello client");
+                        sendMessage("hello client");
                         //UserBuffer.getInstance().add(received);
+                    } else if (inputLine.equals("we are connected")) {
+                        //matchmakerSocket.addClientIP();
+                        System.out.println("getRemoteSocketAddress: "
+                                + clientSocket.getRemoteSocketAddress().toString());
+                        //if ()
+                        sendMessage("You can start game");
                     }
                 }
-
+                System.out.println("Server went out of while loop");
                 in.close();
                 out.close();
                 clientSocket.close();
@@ -68,6 +85,13 @@ public class SocketConnections {
                 Logger.getLogger(SocketConnections.class.getName()).log(Level.SEVERE, null, ex);
             }
 
+        }
+
+        public void sendMessage(String message) throws IOException {
+            out.println(message);
+            //String response = in.readLine();
+            System.out.println("Matchmaker sent message " + message);
+            //return response;
         }
     }
 }
