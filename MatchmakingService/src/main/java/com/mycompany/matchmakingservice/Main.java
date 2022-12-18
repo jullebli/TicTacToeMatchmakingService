cfae9fdb904ffced3ac652cc2e22b5a38695fd39
vornsami <sami.vornanen@helsinki.fi>
2022-12-18 13:25:12 +0200

    Added setting multiple rooms

--- MatchmakingService/src/main/java/com/mycompany/matchmakingservice/Main.java
+++ MatchmakingService/src/main/java/com/mycompany/matchmakingservice/Main.java
@@ -18,9 +18,10 @@ import java.util.Queue;
  * @author bergmjul
  */
 public class Main {
-    private static final int GAME_SIZE = 2;
+    private static final int GAME_SIZE = 3;
     private static final String HOST_LOCATION = "230.0.0.0";
-    private static final String TARGET_LOCATION = "230.0.0.1";
+    private static final String TARGET_LOCATION_BASE = "230.0.0.";
+    private static int numberOfRooms;
     private static boolean RUNNING = true;
     
     public static void main(String[] args) {
@@ -31,16 +32,20 @@ public class Main {
         NetworkPublisher publisher = new MulticastPublisher(HOST_LOCATION);
         
         Queue<String> list = UserBuffer.getInstance();
+        
+        numberOfRooms = 0;
         try {
         while(RUNNING) {
             if(list.size() >= GAME_SIZE) {
                 try {
-                    String message = TARGET_LOCATION;
+                    int tempNumberOfRooms = (numberOfRooms >= 255)? 0 : (numberOfRooms + 1);
+                    String message = TARGET_LOCATION_BASE + tempNumberOfRooms;
                     for(int i=0;i<GAME_SIZE;i++) message += "," + list.poll();
                     
                     publisher.send(message, HOST_LOCATION);
+                    numberOfRooms = tempNumberOfRooms;
                     
-                    System.out.println("Sent game to " + TARGET_LOCATION);
+                    System.out.println("Sent game to " + TARGET_LOCATION_BASE + numberOfRooms);
                     
                 } catch (IOException e) {
                     System.out.println(e);
